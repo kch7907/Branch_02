ca2bd04faa290a35ea8b53530e9a75d99828eb9e
D7703_04 <d7703_04@D7703-04ui-iMac.local>
2017-09-11 14:46:06 +0900

    bc

--- Branch/ViewController.swift
+++ Branch/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Git Test")
         print("real")
-        print("one more2")
+        print("one more5")
     }
 
     override func didReceiveMemoryWarning() {
